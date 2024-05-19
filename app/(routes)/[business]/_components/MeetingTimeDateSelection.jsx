547506b3c2b12335af918142a5bc91e441833507
Joshua <joshu2@MacBook-Air.local>
2024-05-19 09:53:01 +0530

    going back to prev commit

--- app/(routes)/[business]/_components/MeetingTimeDateSelection.jsx
+++ app/(routes)/[business]/_components/MeetingTimeDateSelection.jsx
@@ -1,10 +1,18 @@
-import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
-import { format } from "date-fns";
-import { CalendarCheck, Clock, LoaderIcon, MapPin, Timer } from "lucide-react";
+import {
+  CalendarDays,
+  ChevronLeft,
+  Clock,
+  Clock2,
+  LoaderIcon,
+  MapPin,
+  Timer,
+} from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
+import { Calendar } from "@/components/ui/calendar";
+import { Button } from "@/components/ui/button";
+import { format } from "date-fns";
 import TimeDateSelection from "./TimeDateSelection";
 import UserFormInfo from "./UserFormInfo";
 import {
@@ -22,61 +30,55 @@ import { useRouter } from "next/navigation";
 import Plunk from "@plunk/node";
 import { render } from "@react-email/render";
 import Email from "@/emails";
-function MeetingTimeDateSelection({ eventInfo, businessInfo }) {
+
+const MeetingTimeDateSelection = ({ eventInfo, businessInfo }) => {
   const [date, setDate] = useState(new Date());
   const [timeSlots, setTimeSlots] = useState();
-  const [enableTimeSlot, setEnabledTimeSlot] = useState(false);
+  const [enableTimeSlot, setEnableTimeSlot] = useState(false);
   const [selectedTime, setSelectedTime] = useState();
   const [userName, setUserName] = useState();
   const [userEmail, setUserEmail] = useState();
   const [userNote, setUserNote] = useState("");
-  const [prevBooking, setPrevBooking] = useState([]);
   const [step, setStep] = useState(1);
-  const router = useRouter();
-  const db = getFirestore(app);
   const [loading, setLoading] = useState(false);
+  const [prevBooking, setPrevBooking] = useState([]);
+  const router = useRouter();
+
+  const db = getFirestore(app);
   const plunk = new Plunk(process.env.NEXT_PUBLIC_PLUNK_API_KEY);
+
   useEffect(() => {
     eventInfo?.duration && createTimeSlot(eventInfo?.duration);
   }, [eventInfo]);
+
   const createTimeSlot = (interval) => {
-    const startTime = 8 * 60; // 8 AM in minutes
-    const endTime = 22 * 60; // 10 PM in minutes
+    const startTime = 8 * 60; //8am in min
+    const endTime = 22 * 60; // 10pm in min
     const totalSlots = (endTime - startTime) / interval;
     const slots = Array.from({ length: totalSlots }, (_, i) => {
       const totalMinutes = startTime + i * interval;
       const hours = Math.floor(totalMinutes / 60);
       const minutes = totalMinutes % 60;
-      const formattedHours = hours > 12 ? hours - 12 : hours; // Convert to 12-hour format
+      const formattedHours = hours > 12 ? hours - 12 : hours; //convert to 12-hour format
       const period = hours >= 12 ? "PM" : "AM";
       return `${String(formattedHours).padStart(2, "0")}:${String(
         minutes
       ).padStart(2, "0")} ${period}`;
     });
-
-    console.log(slots);
     setTimeSlots(slots);
   };
 
-  /**
-   * On Date Change Handle Method
-   * @param {*} date
-   */
   const handleDateChange = (date) => {
     setDate(date);
     const day = format(date, "EEEE");
     if (businessInfo?.daysAvailable?.[day]) {
       getPrevEventBooking(date);
-      setEnabledTimeSlot(true);
+      setEnableTimeSlot(true);
     } else {
-      setEnabledTimeSlot(false);
+      setEnableTimeSlot(false);
     }
   };
 
-  /**
-   * Handle Schedule Event on Click Schedule Button
-   * @returns
-   */
   const handleScheduleEvent = async () => {
     const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     if (regex.test(userEmail) == false) {
@@ -84,6 +86,7 @@ function MeetingTimeDateSelection({ eventInfo, businessInfo }) {
       return;
     }
     const docId = Date.now().toString();
+    // add data to firebase
     setLoading(true);
     await setDoc(doc(db, "ScheduledMeetings", docId), {
       businessName: businessInfo.businessName,
@@ -100,15 +103,13 @@ function MeetingTimeDateSelection({ eventInfo, businessInfo }) {
       userEmail: userEmail,
       userNote: userNote,
     }).then((resp) => {
-      toast("Meeting Scheduled successfully!");
+      setLoading(false);
+      // toast("Meeting scheduled successfully!");
+      router.replace("/confirmation");
       sendEmail(userName);
     });
   };
 
-  /**
-   * Used to Send an email to User
-   * @param {*} user
-   */
   const sendEmail = (user) => {
     const emailHtml = render(
       <Email
@@ -124,68 +125,99 @@ function MeetingTimeDateSelection({ eventInfo, businessInfo }) {
     plunk.emails
       .send({
         to: userEmail,
-        subject: "Meeting Schedul Details",
+        subject: "Meeting schedule details",
         body: emailHtml,
       })
       .then((resp) => {
         console.log(resp);
-        setLoading(false);
         router.replace("/confirmation");
       });
   };
 
-  /**
-   * Used to Fetch Previous Booking for given event
-   * @param {*} date_
-   */
+  //used to fetch prev booking for given event
   const getPrevEventBooking = async (date_) => {
     const q = query(
       collection(db, "ScheduledMeetings"),
       where("selectedDate", "==", date_),
       where("eventId", "==", eventInfo.id)
     );
-
     const querySnapshot = await getDocs(q);
-
     querySnapshot.forEach((doc) => {
       console.log("--", doc.data());
       setPrevBooking((prev) => [...prev, doc.data()]);
     });
   };
+
   return (
     <div
-      className="p-5 py-10 shadow-lg m-5 border-t-8
-    mx-10
-    md:mx-26
-    lg:mx-56
-    my-10"
+      className="p-5 py-10 shadow-lg m-8 border-t-8 mx-10 md:mx-26 lg:mx-56 my-10"
       style={{ borderTopColor: eventInfo?.themeColor }}
     >
-      <Image src="/logo.svg" alt="logo" width={150} height={150} />
+      {/* TOP */}
+      <div className="flex justify-between">
+        {/* logo */}
+        <Image
+          src={"/logo.svg"}
+          height={40}
+          width={80}
+          alt="logo"
+          className="ml-5"
+        />
+        {/* Schedule meeting, back, next button */}
+        <div className="flex">
+          {step == 2 && (
+            <Button
+              className="m-1 mt-5 px-4 py-3 flex"
+              variant="outline"
+              onClick={() => setStep(1)}
+            >
+              <ChevronLeft /> Back
+            </Button>
+          )}
+          {step == 1 ? (
+            <Button
+              className="m-1 mt-5 px-5 py-3"
+              disabled={!selectedTime || !date}
+              onClick={() => setStep(step + 1)}
+            >
+              Next
+            </Button>
+          ) : (
+            <Button
+              className="m-1 mt-5 px-7 py-3"
+              disabled={!userEmail || !userName}
+              onClick={handleScheduleEvent}
+            >
+              {loading ? <LoaderIcon className="animate-spin" /> : "Schedule"}
+            </Button>
+          )}
+        </div>
+      </div>
+      {/* MIDDLE */}
       <div className="grid grid-cols-1 md:grid-cols-3 mt-5">
-        {/* Meeting Info  */}
+        {/* LEFT SIDE: meeting info */}
         <div className="p-4 border-r">
           <h2>{businessInfo?.businessName}</h2>
-          <h2 className="font-bold text-3xl">
+          <h2 className="font-bold text-2xl">
             {eventInfo?.eventName ? eventInfo?.eventName : "Meeting Name"}
           </h2>
           <div className="mt-5 flex flex-col gap-4">
             <h2 className="flex gap-2">
               <Clock />
-              {eventInfo?.duration} Min{" "}
+              {eventInfo?.duration} Min
             </h2>
             <h2 className="flex gap-2">
               <MapPin />
               {eventInfo?.locationType} Meeting{" "}
             </h2>
             <h2 className="flex gap-2">
-              <CalendarCheck />
-              {format(date, "PPP")}{" "}
+              <CalendarDays />
+              {format(date, "PPP")}
             </h2>
             {selectedTime && (
               <h2 className="flex gap-2">
                 <Timer />
-                {selectedTime}{" "}
+                {selectedTime}
               </h2>
             )}
 
@@ -197,14 +229,14 @@ function MeetingTimeDateSelection({ eventInfo, businessInfo }) {
             </Link>
           </div>
         </div>
-        {/* Time & Date Selction  */}
+        {/* RIGHT SIDE: time and date selection */}
         {step == 1 ? (
           <TimeDateSelection
             date={date}
-            enableTimeSlot={enableTimeSlot}
             handleDateChange={handleDateChange}
-            setSelectedTime={setSelectedTime}
             timeSlots={timeSlots}
+            setSelectedTime={setSelectedTime}
+            enableTimeSlot={enableTimeSlot}
             selectedTime={selectedTime}
             prevBooking={prevBooking}
           />
@@ -216,31 +248,8 @@ function MeetingTimeDateSelection({ eventInfo, businessInfo }) {
           />
         )}
       </div>
-      <div className="flex gap-3 justify-end">
-        {step == 2 && (
-          <Button variant="outline" onClick={() => setStep(1)}>
-            Back
-          </Button>
-        )}
-        {step == 1 ? (
-          <Button
-            className="mt-10 float-right"
-            disabled={!selectedTime || !date}
-            onClick={() => setStep(step + 1)}
-          >
-            Next
-          </Button>
-        ) : (
-          <Button
-            disabled={!userEmail || !userName}
-            onClick={handleScheduleEvent}
-          >
-            {loading ? <LoaderIcon className="animate-spin" /> : "Schedule"}
-          </Button>
-        )}
-      </div>
     </div>
   );
-}
+};
 
 export default MeetingTimeDateSelection;
